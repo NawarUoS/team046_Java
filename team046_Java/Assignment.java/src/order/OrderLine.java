a04fe2feb91d3e5846e21408112f8940d1dcf8b3
Nawar <125588581+NawarUoS@users.noreply.github.com>
2023-11-13 20:57:42 +0000

    Fixed a broken push

--- team046_Java/Assignment.java/src/order/OrderLine.java
+++
@@ -1,9 +0,0 @@
-package src.order;
-
-import src.product.*;
-
-public class OrderLine {
-    private int orderLineNumber;
-    private Product item;
-    private int quantity;
-}
--- team046_Java/Assignment.java/src/product/TrackPack.java
+++
@@ -1,7 +0,0 @@
-package src.product;
-
-import java.util.List;
-
-public class TrackPack extends BoxedSet {
-
-}
