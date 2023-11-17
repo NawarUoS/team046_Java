a9e299beb943f1d903e62a258e08b4e5342ab4f4
Ethan Holloway <eholloway1@sheffield.ac.uk>
2023-11-17 19:13:11 +0000

    Tidied up product folder

--- Assignment.java/src/product/TrainSet.java
+++ Assignment.java/src/product/TrainSet.java
@@ -2,17 +2,26 @@ package src.product;
 
 import java.util.List;
 
-public class TrainSet extends BoxedSet {
+public class TrainSet extends Product {
     private enum eraCode {};
     private String trainSetType;
     private List<String> contents;
 
-    //Get methods
+    //Getters
     public String getTrainSetType() {
         return trainSetType;
     }
 
     public List<String> getContents() {
         return contents;
+    }
+
+    //Setters
+    public void setTrainSetType(String trainSetType) {
+        this.trainSetType = trainSetType;
+    }
+
+    public void setContents(List<String> contents) {
+        this.contents = contents;
     }
 }
--- Assignment.java/src/product/Wagon.java
+++
@@ -1,5 +0,0 @@
-package src.product;
-
-public class Wagon extends RollingStock {
-    
-}
