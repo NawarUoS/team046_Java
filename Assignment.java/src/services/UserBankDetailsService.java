93b6c5b1947bb35c58c6d0002c7e9e4cbc1b2c6b
NawarUoS <naboutrabi1@sheffield.ac.uk>
2023-11-20 11:33:40 +0000

    Minor commit - added updateBankDetails function signature in UserBankDetailsService

--- Assignment.java/src/services/UserBankDetailsService.java
+++ Assignment.java/src/services/UserBankDetailsService.java
@@ -5,7 +5,8 @@ import src.account.*;
 public class UserBankDetailsService extends Service {
     private int currentUserID;
 
-    public void updateBankDetails() {
+    public void updateBankDetails(String cardName, String cardHolder,
+                      int[] cardNumber, String expiryDate, int securityCode) {
 
     }
 }
