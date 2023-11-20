d49b657c3515bd083c8685a20524e90c63d5982b
Ibrahim Adeel <iahmed7@sheffield.ac.uk>
2023-11-20 10:57:53 +0000

    shopping cart service started

--- Assignment.java/src/services/ShoppingCartService.java
+++ Assignment.java/src/services/ShoppingCartService.java
@@ -1,4 +1,12 @@
 package src.services;
 
+import src.order.OrderLine;
+
 public class ShoppingCartService {
+    private int currentUserID;
+    private list<OrderLine> cartItems;
+
+    public Product addItem() {
+
+    }
 }
