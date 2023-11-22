51c86b63188c053c5bc054f87219384b9d52a69e
NawarUoS <naboutrabi1@sheffield.ac.uk>
2023-11-22 12:36:05 +0000

    WIP : LoginOperations

--- Assignment.java/src/connection/LoginOperations.java
+++ Assignment.java/src/connection/LoginOperations.java
@@ -7,21 +7,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+
+// Most of this code was inspired by COM2008 Week 08 Lab 5 solution
 public class LoginOperations {
-    public String verifyLogin(Connection connection, String username,
+    public String verifyLogin(Connection connection, String emailAddress,
                                                     String enteredPassword) {
         try {
             // Query the database to fetch user information
-            String sql = "SELECT userId, password_hash, " +
-                    "FROM Users WHERE username = ?";
+            String sql = "SELECT userID, unique_password_hash, " +
+                    "FROM Accounts WHERE emailAddress = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, username);
+            statement.setString(1, emailAddress);
+            System.out.print(statement);
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
                 String userId = resultSet.getString("userId");
                 String storedPasswordHash = resultSet.getString(
-                        "password_hash");
+                        "unique_password_hash");
+
+                if (verifyPassword(enteredPassword.toCharArray(),
+                        storedPasswordHash)) {
+                    return "Login successful for user: " + emailAddress;
+                } else {
+                    return "Incorrect password.";
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
