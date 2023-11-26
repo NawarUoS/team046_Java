cdb5dbc13357f4487d461c4852f4271315e747f8
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-11-26 23:02:56 +0000

    implemented save and update methods in OrderLineOperations

--- Assignment.java/src/model/OrderLineOperations.java
+++ Assignment.java/src/model/OrderLineOperations.java
@@ -9,7 +9,7 @@ import src.order.OrderLine;
 
 public class OrderLineOperations {
 
-    public OrderLine getOrderLineByOrderLineNumber(Connection connection, int order_line_number) {
+    public OrderLine getOrderLineByOrderLineNumber(Connection connection, int order_line_number) throws Error {
         try {
             // Query the database to fetch user information
             String sql = "SELECT item_quantity, order_line_cost, product_code, order_number FROM OrderLines " +
@@ -33,4 +33,92 @@ public class OrderLineOperations {
         throw new Error("Order line not found.");
     }
 
+    public String saveOrderLineInDatabase(Connection connection, OrderLine orderLine) {
+        int orderLineNumber = orderLine.getOrderLineNumber();
+        String productCode = orderLine.getProductCode();
+        int quantity = orderLine.getQuantity();
+        double orderLineCost = orderLine.getOrderLineCost();
+        int orderNumber = orderLine.getOrderNumber();
+
+        // Cancels operation if it finds a duplicate
+        if (checkOrderLineInDatabase(connection, orderLineNumber)) {
+            return "Order line of this order already exists.";
+        }
+        try {
+            // Query the database to insert entry
+            String sql = "INSERT INTO OrderLines (order_line_number," + 
+            "item_quantity, order_line_cost, product_code, order_number) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            // Set parameters for the query
+            statement.setInt(1, orderLineNumber);
+            statement.setInt(2, quantity);
+            statement.setDouble(3, orderLineCost);
+            statement.setString(4, productCode);
+            statement.setInt(5, orderNumber);
+            
+            // Execute the insert statement
+            statement.executeUpdate();
+
+            // Close the statement to release resources
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Successfully added order line!";
+    }
+
+    public boolean checkOrderLineInDatabase(Connection connection, int orderLineNumber) {
+        try {
+            // TODO check if checking the same value works 
+            String sql = "SELECT order_line_number FROM OrderLines WHERE order_line_number = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, orderLineNumber);
+            ResultSet resultSet = statement.executeQuery();
+
+            // Account exists if result set is not empty
+            if (resultSet.next()) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        return false;
+    }
+
+    // Refrences order line number to update 
+    public String updateAccountDetails(
+                        Connection connection, int orderLineNumber, 
+                        int quantity, double orderLineCost, String productCode, 
+                        int orderNumber) {
+
+        // Cancels operation if order line number does not exist
+        if (!checkOrderLineInDatabase(connection, orderLineNumber))
+            return "Order line does not exist. Couldn't update details.";
+        try {
+            // Query the database to update details
+            String sql = "UPDATE OrderLines SET item_quantity = ?, " +
+                "order_line_cost = ?, product_code = ?, order_number = ?" +
+                "WHERE order_line_number = ?";
+
+            // Set parameters for the query
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, orderLineNumber);
+            statement.setInt(2, quantity);
+            statement.setDouble(3, orderLineCost);
+            statement.setString(4, productCode);
+            statement.setInt(6, orderNumber);
+
+            // Executes the update statement
+            statement.executeUpdate();
+
+            // Close the statement to release resources
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Successfully updated order line details!";
+    }
 }
