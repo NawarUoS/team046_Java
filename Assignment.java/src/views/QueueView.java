c5a544bdb852e29b876cf81cbff61dddeff36e7f
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-11-28 19:46:47 +0000

    Implemented functionality for QueueView

--- Assignment.java/src/views/QueueView.java
+++ Assignment.java/src/views/QueueView.java
@@ -8,6 +8,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class QueueView extends JPanel {
     private Connection connection;
     private CardLayout cardLayout;
@@ -31,10 +34,10 @@ public class QueueView extends JPanel {
 
         // Buttons to delete an order and change the status of an order
         JButton deleteButton = new JButton("Delete Order");
-        deleteButton.addActionListener(e -> deleteOrder());
+        deleteButton.addActionListener(e -> deleteOrder(table));
 
         JButton changeStatusButton = new JButton("Change Status");
-        changeStatusButton.addActionListener(e -> changeOrderStatus());
+        changeStatusButton.addActionListener(e -> changeOrderStatus(table));
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(backButton);
@@ -72,15 +75,103 @@ public class QueueView extends JPanel {
         return new JTable(model);
     }
 
-    private void deleteOrder() {
-        // Implement the logic to delete an order
-        // You can use the selected row in the table to determine the order to delete
-        // For example, int selectedRow = table.getSelectedRow();
-    }
+    private void deleteOrder(JTable table) {
+        int selectedRow = table.getSelectedRow();
+    
+        if (selectedRow != -1) { // Check if a row is selected
+            int orderNumber = (int) table.getValueAt(selectedRow, 0);
+    
+            // Confirm the deletion with a dialog box
+            int option = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this order?", "Confirm Deletion", JOptionPane.YES_NO_OPTION);
+    
+            if (option == JOptionPane.YES_OPTION) {
+                // User confirmed deletion
+                try {
+                    // Example SQL query to delete the order
+                    String deleteQuery = "DELETE FROM Orders WHERE order_number = ?";
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
+                        preparedStatement.setInt(1, orderNumber);
+                        preparedStatement.executeUpdate();
+                    }
+    
+                    // Refresh the table after deletion
+                    refreshTable(table);
+    
+                    // Display a confirmation message
+                    JOptionPane.showMessageDialog(this, "Order deleted successfully.", "Deletion Successful", JOptionPane.INFORMATION_MESSAGE);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select a row to delete.", "Delete Order", JOptionPane.WARNING_MESSAGE);
+        }
+    }        
+    private void changeOrderStatus(JTable table) {
+        int[] selectedRows = table.getSelectedRows();
 
-    private void changeOrderStatus() {
-        // Implement the logic to change the status of an order
-        // You can use the selected row in the table to determine the order to update
-        // For example, int selectedRow = table.getSelectedRow();
-    }
+        if (selectedRows.length > 0) { // Check if at least one row is selected
+            List<Integer> orderNumbers = new ArrayList<>();
+
+            // Prompt the user for the new order status
+            String newStatus = JOptionPane.showInputDialog(this, "Enter new status (p, c, or f):");
+
+            if (newStatus != null && (newStatus.equals("p") || newStatus.equals("c") || newStatus.equals("f"))) {
+                // Collect order numbers for selected rows
+                for (int selectedRow : selectedRows) {
+                    int orderNumber = (int) table.getValueAt(selectedRow, 0);
+                    orderNumbers.add(orderNumber);
+                }
+
+                // Implement the logic to change the status of the selected orders
+                try {
+                    // Example SQL query to update the order status for multiple orders
+                    String updateQuery = "UPDATE Orders SET order_status = ? WHERE order_number IN (" +
+                            String.join(",", orderNumbers.stream().map(String::valueOf).toArray(String[]::new)) + ")";
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+                        preparedStatement.setString(1, newStatus);
+                        preparedStatement.executeUpdate();
+                    }
+
+                    // Refresh the table after status change
+                    refreshTable(table);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid status. Please enter p, c, or f.", "Change Order Status", JOptionPane.WARNING_MESSAGE);
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select at least one row to change the status.", "Change Order Status", JOptionPane.WARNING_MESSAGE);
+        }
+    }        
+    private void refreshTable(JTable table) {
+        // Clear the existing rows
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        model.setRowCount(0);
+    
+        // Repopulate the table with updated data
+        try {
+            // SQL query to retrieve data from your table
+            String sqlQuery = "SELECT order_number, order_date, total_cost, order_status, userID FROM Orders";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+    
+                while (resultSet.next()) {
+                    Object[] row = {
+                            resultSet.getInt("order_number"),
+                            resultSet.getDate("order_date"), // Adjust the data type if necessary
+                            resultSet.getDouble("total_cost"),
+                            resultSet.getString("order_status"),
+                            resultSet.getInt("userID")
+                    };
+                    model.addRow(row);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    
+        model.fireTableDataChanged();
+    }    
 }
