2745fe697705a190d383a29aa5916ad3bb78a3b2
Ibrahim Adeel <iahmed7@sheffield.ac.uk>
2023-11-28 22:19:03 +0000

    CartView

--- Assignment.java/src/views/CartView.java
+++ Assignment.java/src/views/CartView.java
@@ -1,42 +1,65 @@
-
 package src.views;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.*;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
 
 public class CartView extends JFrame {
-    private DefaultListModel<String> cartListModel;
-    private JList<String> cartList;
-    private JButton removeButton;
+    // Components
+    private JList<String> cartItemList;
+    private JButton checkoutButton;
+    private JButton removeItemButton;
 
-    private Connection connection;
-
-    public CartView(Connection connection) throws SQLException {
+    public CartView(Connection connection) {
+        // GUI Initialization
         this.setTitle("Shopping Cart");
-        this.setSize(400, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(400, 400));
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        cartListModel = new DefaultListModel<>();
-        cartList = new JList<>(cartListModel);
-        JScrollPane scrollPane = new JScrollPane(cartList);
-        add(scrollPane, BorderLayout.CENTER);
+        JPanel panel = new JPanel();
+        this.add(panel);
 
-        removeButton = new JButton("Remove Item");
-        removeButton.addActionListener(e -> {
-            int selectedIndex = cartList.getSelectedIndex();
-            if (selectedIndex != -1) {
-                cartListModel.remove(selectedIndex);
-            }
-        });
-        add(removeButton, BorderLayout.SOUTH);
+        panel.setLayout(new GridLayout(12, 2, 10, 10));
+
+        // Create a list to display cart items
+        DefaultListModel<String> cartListModel = new DefaultListModel<>();
+        cartItemList = new JList<>(cartListModel);
+        JScrollPane scrollPane = new JScrollPane(cartItemList);
+        panel.add(scrollPane, BorderLayout.CENTER);
+
+        // Create Buttons
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        checkoutButton = new JButton("Checkout");
+        removeItemButton = new JButton("Remove Item");
+        buttonPanel.add(removeItemButton);
+        buttonPanel.add(checkoutButton);
+
+        panel.add(buttonPanel, BorderLayout.SOUTH);
     }
 
+    // Add a method to update the cart items in the JList
+    public void updateCartItems(String[] items) {
+        DefaultListModel<String> cartListModel = (DefaultListModel<String>) cartItemList.getModel();
+        cartListModel.clear();
+        for (String item : items) {
+            cartListModel.addElement(item);
+        }
+    }
 
+    // Add action listeners for checkout and remove item buttons
 
+    public void addCheckoutButtonListener(ActionListener listener) {
+        checkoutButton.addActionListener(listener);
+    }
 
-
+    public void addRemoveItemButtonListener(ActionListener listener) {
+        removeItemButton.addActionListener(listener);
+    }
 }
+
+
 
 
 
