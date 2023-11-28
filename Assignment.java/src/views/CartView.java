670ee2e14bb2deb7d3e22c54157b7aed0033c00b
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-11-28 23:27:01 +0000

    Implemented CartView gui (with Ibrahim)

--- Assignment.java/src/views/CartView.java
+++ Assignment.java/src/views/CartView.java
@@ -1,115 +1,115 @@
-
-
-
-
-
 package src.views;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class ProfileView extends JFrame {
-    // Components
-    private JTextField firstNameField;
-    private JTextField lastNameField;
-    private JTextField emailField;
-    private JTextField houseNumberField;
-    private JTextField roadNameField;
-    private JTextField cityNameField;
-    private JTextField postcodeField;
-    private JTextField cardNumberField;
-    private JTextField cardHolderField;
-    private JTextField cardExpiryField;
-    private JTextField securityCodeField;
+public class CartView extends JFrame {
+    private Connection connection;
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    private int orderNumber; // Assuming each CartView is associated with a unique order_number
+    private JTable table;
 
-    public ProfileView() {
-        // GUI Initialization
-        this.setTitle("Customer Profile");
+    public CartView(Connection connection, int orderNumber) {
+        // Create the JFrame in the constructor
+        this.setTitle("Cart");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(new Dimension(400, 400));
+        this.setSize(800, 500);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+        this.connection = connection;
+        this.orderNumber = orderNumber;
 
-        JPanel panel = new JPanel();
-        this.add(panel);
+        setLayout(new BorderLayout());
 
-        panel.setLayout(new GridLayout(12, 2, 10, 10));
+        // Initialize CardLayout and JPanel
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
 
-        // Create Labels and Text Fields
-        JLabel firstNameLabel = new JLabel("First Name:");
-        firstNameField = new JTextField(20);
+        // Content for Cart Screen
+        add(new JLabel("Cart Screen Content", SwingConstants.CENTER));
 
-        JLabel lastNameLabel = new JLabel("Last Name:");
-        lastNameField = new JTextField(20);
+        // Back button to the initial screen
+        JButton backButton = new JButton("Back to Main Screen");
+        backButton.addActionListener(e -> cardLayout.show(cardPanel, "Initial"));
 
-        JLabel emailLabel = new JLabel("Email:");
-        emailField = new JTextField(20);
+        // Table with data from the SQL table OrderLines
+        table = createTableFromSQL();
+        JScrollPane scrollPane = new JScrollPane(table);
 
-        JLabel houseNumberLabel = new JLabel("House Number:");
-        houseNumberField = new JTextField(20);
+        // Buttons
+        
+        JButton editQuantityButton = new JButton("Edit Quantity");
+        editQuantityButton.addActionListener(this::editQuantity);
+        
+        JButton removeButton = new JButton("Remove");
+        removeButton.addActionListener(this::removeOrderLine);
+        
+        JButton confirmOrderButton = new JButton("Confirm Order");
+        confirmOrderButton.addActionListener(this::confirmOrder);
 
-        JLabel roadNameLabel = new JLabel("Road Name:");
-        roadNameField = new JTextField(20);
 
-        JLabel cityNameLabel = new JLabel("City Name:");
-        cityNameField = new JTextField(20);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(backButton);
+        buttonPanel.add(removeButton);
+        buttonPanel.add(editQuantityButton);
+        buttonPanel.add(confirmOrderButton);
 
-        JLabel postcodeLabel = new JLabel("Postcode:");
-        postcodeField = new JTextField(20);
-
-        JLabel cardNumberLabel = new JLabel("Card Number:");
-        cardNumberField = new JTextField(20);
-
-        JLabel cardHolderLabel = new JLabel("Cardholder Name:");
-        cardHolderField = new JTextField(20);
-
-        JLabel cardExpiryLabel = new JLabel("Card Expiry (mm/yy):");
-        cardExpiryField = new JTextField(20);
-
-        JLabel securityCodeLabel = new JLabel("Security Code:");
-        securityCodeField = new JTextField(20);
-
-        // Button to Save Changes
-        JButton saveButton = new JButton("Save");
-//            saveButton.addActionListener(new ActionListener() {
-//                @Override
-//                public void actionPerformed(ActionEvent e) {
-//                    // Call method to update details in the database
-//                    updateDetails();
-//                }
-//            });
-
-        // Add components to the frame
-        panel.add(firstNameLabel);
-        panel.add(firstNameField);
-        panel.add(lastNameLabel);
-        panel.add(lastNameField);
-        panel.add(emailLabel);
-        panel.add(emailField);
-        panel.add(houseNumberLabel);
-        panel.add(houseNumberField);
-        panel.add(roadNameLabel);
-        panel.add(roadNameField);
-        panel.add(cityNameLabel);
-        panel.add(cityNameField);
-        panel.add(postcodeLabel);
-        panel.add(postcodeField);
-        panel.add(cardNumberLabel);
-        panel.add(cardNumberField);
-        panel.add(cardHolderLabel);
-        panel.add(cardHolderField);
-        panel.add(cardExpiryLabel);
-        panel.add(cardExpiryField);
-        panel.add(securityCodeLabel);
-        panel.add(securityCodeField);
-        panel.add(saveButton);
-
+        add(scrollPane, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
+    private JTable createTableFromSQL() {
+        String[] columnNames = {"Order Line Number", "Item Quantity", "Order Line Cost", "Product Code", "Order Number"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+        // SQL query to retrieve data from the OrderLines table for a specific order_number
+        String sqlQuery = "SELECT order_line_number, items_quantity, order_line_cost, product_code, order_number FROM OrderLines WHERE order_number = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            preparedStatement.setInt(1, orderNumber);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Object[] row = {
+                        resultSet.getInt("order_line_number"),
+                        resultSet.getInt("items_quantity"),
+                        resultSet.getDouble("order_line_cost"),
+                        resultSet.getString("product_code"),
+                        resultSet.getInt("order_number")
+                };
+                model.addRow(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return new JTable(model);
+    }
+
+
+    private void confirmOrder(ActionEvent event) {
+        // Placeholder method, replace with actual logic to confirm the order
+        JOptionPane.showMessageDialog(this, "Order confirmed successfully.", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void editQuantity(ActionEvent event) {
+        // Placeholder method, replace with logic to edit the quantity
+        JOptionPane.showMessageDialog(this, "Editing quantity...", "Edit Quantity", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void removeOrderLine(ActionEvent event) {
+        // Placeholder method, replace with logic to remove the order line
+        JOptionPane.showMessageDialog(this, "Removing order line...", "Remove Order Line", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void refreshTable() {
+        // Same as before
+    }
 }
-
-
-
-
-
-
