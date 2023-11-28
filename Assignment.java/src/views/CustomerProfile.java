976c1f64031f3960d46f0a78ddfb9b52e05c205a
Ibrahim Adeel <iahmed7@sheffield.ac.uk>
2023-11-28 18:21:52 +0000

    New code for customer profile and cart

--- Assignment.java/src/views/CustomerProfile.java
+++ Assignment.java/src/views/CustomerProfile.java
@@ -9,119 +9,84 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-    public class CustomerProfile extends JFrame {
-        // Components
-        private JTextField firstNameField, lastNameField, emailField, addressField, cardNumberField, cardHolderField, cardExpiryField, securityCodeField;
+public class CustomerProfile extends JFrame {
+    // Components
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField emailField;
+    private JTextField addressField;
+    private JTextField cardNumberField;
+    private JTextField cardHolderField;
+    private JTextField cardExpiryField;
+    private JTextField securityCodeField;
 
-        public CustomerProfile(Connection connection)  throws SQLException {
-            // GUI Initialization
-            this.setTitle("Customer Profile");
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setLayout(new GridLayout(8, 2, 10, 10));
-            setPreferredSize(new Dimension(400, 400));
+    public CustomerProfile() {
+        // GUI Initialization
+        this.setTitle("Customer Profile");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(new Dimension(400, 400));
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
 
-            // Create Labels and Text Fields
-            JLabel firstNameLabel = new JLabel("First Name:");
-            firstNameField = new JTextField(20);
+        JPanel panel = new JPanel();
+        this.add(panel);
 
-            JLabel lastNameLabel = new JLabel("Last Name:");
-            lastNameField = new JTextField(20);
+        panel.setLayout(new GridLayout(12, 2, 10, 10));
 
-            JLabel emailLabel = new JLabel("Email:");
-            emailField = new JTextField(20);
+        // Create Labels and Text Fields
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameField = new JTextField(20);
 
-            JLabel addressLabel = new JLabel("Address:");
-            addressField = new JTextField(20);
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        lastNameField = new JTextField(20);
 
-            JLabel cardNumberLabel = new JLabel("Card Number:");
-            cardNumberField = new JTextField(20);
+        JLabel emailLabel = new JLabel("Email:");
+        emailField = new JTextField(20);
 
-            JLabel cardHolderLabel = new JLabel("Cardholder Name:");
-            cardHolderField = new JTextField(20);
+        JLabel addressLabel = new JLabel("Address:");
+        addressField = new JTextField(20);
 
-            JLabel cardExpiryLabel = new JLabel("Card Expiry (mm/yy):");
-            cardExpiryField = new JTextField(20);
+        JLabel cardNumberLabel = new JLabel("Card Number:");
+        cardNumberField = new JTextField(20);
 
-            JLabel securityCodeLabel = new JLabel("Security Code:");
-            securityCodeField = new JTextField(20);
+        JLabel cardHolderLabel = new JLabel("Cardholder Name:");
+        cardHolderField = new JTextField(20);
 
-            // Button to Save Changes
-            JButton saveButton = new JButton("Save");
-            saveButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Call method to update details in the database
-                    updateDetails();
-                }
-            });
+        JLabel cardExpiryLabel = new JLabel("Card Expiry (mm/yy):");
+        cardExpiryField = new JTextField(20);
 
-            // Add components to the frame
-            add(firstNameLabel);
-            add(firstNameField);
-            add(lastNameLabel);
-            add(lastNameField);
-            add(emailLabel);
-            add(emailField);
-            add(addressLabel);
-            add(addressField);
-            add(cardNumberLabel);
-            add(cardNumberField);
-            add(cardHolderLabel);
-            add(cardHolderField);
-            add(cardExpiryLabel);
-            add(cardExpiryField);
-            add(securityCodeLabel);
-            add(securityCodeField);
-            add(saveButton);
+        JLabel securityCodeLabel = new JLabel("Security Code:");
+        securityCodeField = new JTextField(20);
 
-            pack();
-            setLocationRelativeTo(null);
-            setVisible(true);
-        }
+        // Button to Save Changes
+        JButton saveButton = new JButton("Save");
+//            saveButton.addActionListener(new ActionListener() {
+//                @Override
+//                public void actionPerformed(ActionEvent e) {
+//                    // Call method to update details in the database
+//                    updateDetails();
+//                }
+//            });
 
-        // Method to update customer details in the database
-        private void updateDetails() {
-            String fname = firstNameField.getText();
-            String lname = lastNameField.getText();
-            String email = emailField.getText();
-            String address = addressField.getText();
-            String cardNumber = cardNumberField.getText();
-            String cardHolder = cardHolderField.getText();
-            String cardExpiry = cardExpiryField.getText();
-            String securityCode = securityCodeField.getText();
-
-            // Database connection and update
-            try {
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yourDatabase", "username", "password");
-                String sql = "UPDATE something SET fname=?, lname=?, email=?, address=?, card_number=?, card_holder=?, card_expiry=?, security_code=? WHERE something=?";
-                PreparedStatement statement = connection.prepareStatement(sql);
-
-                // Set parameters
-                statement.setString(1, fname);
-                statement.setString(1, lname);
-                statement.setString(2, email);
-                statement.setString(3, address);
-                statement.setString(4, cardNumber);
-                statement.setString(5, cardHolder);
-                statement.setString(6, cardExpiry);
-                statement.setString(7, securityCode);
-                // Set customer_id based on logged-in user or other identifier
-
-                int rowsUpdated = statement.executeUpdate();
-
-                if (rowsUpdated > 0) {
-                    JOptionPane.showMessageDialog(this, "Details updated successfully!");
-                } else {
-                    JOptionPane.showMessageDialog(this, "Failed to update details.");
-                }
-
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        // Add components to the frame
+        panel.add(firstNameLabel);
+        panel.add(firstNameField);
+        panel.add(lastNameLabel);
+        panel.add(lastNameField);
+        panel.add(emailLabel);
+        panel.add(emailField);
+        panel.add(addressLabel);
+        panel.add(addressField);
+        panel.add(cardNumberLabel);
+        panel.add(cardNumberField);
+        panel.add(cardHolderLabel);
+        panel.add(cardHolderField);
+        panel.add(cardExpiryLabel);
+        panel.add(cardExpiryField);
+        panel.add(securityCodeLabel);
+        panel.add(securityCodeField);
+        panel.add(saveButton);
 
     }
 
-
+}
