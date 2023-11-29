b4ff371db1526b2bb3dd1d866dbe048d69ca3cc3
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-11-29 17:10:08 +0000

    Minor change in change status button QueueView

--- Assignment.java/src/views/QueueView.java
+++ Assignment.java/src/views/QueueView.java
@@ -38,8 +38,8 @@ public class QueueView extends JPanel {
         JButton deleteButton = new JButton("Delete Order");
         deleteButton.addActionListener(e -> deleteOrder(table));
 
-        JButton changeStatusButton = new JButton("Change Status");
-        changeStatusButton.addActionListener(e -> changeOrderStatus(table));
+        JButton fulfillOrderButton = new JButton("Fulfill Order");
+        fulfillOrderButton.addActionListener(e -> fulfillOrder(table));
 
         // Refresh button
         JButton refreshButton = new JButton("Refresh");
@@ -48,27 +48,28 @@ public class QueueView extends JPanel {
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(backButton);
         buttonPanel.add(deleteButton);
-        buttonPanel.add(changeStatusButton);
+        buttonPanel.add(fulfillOrderButton);
         buttonPanel.add(refreshButton);
 
         add(scrollPane, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
     }
+
     private JTable createTableFromSQL() {
-        String[] columnNames = {"Order Number", "Date", "Customer Info", "Email", "Postal Address", "Order Contents", "Order Cost", "Order Status", "Valid Bank Card"};
+        String[] columnNames = { "Order Number", "Date", "Customer Info", "Email", "Postal Address", "Order Contents",
+                "Order Cost", "Order Status", "Valid Bank Card" };
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
 
         // SQL query to retrieve confirmed orders with relevant information
         String sqlQuery = "SELECT o.order_number, o.order_date, a.forename, a.surname, a.email_address, ad.house_number, ad.street_name, ad.city_name, ad.postcode, "
-        + "GROUP_CONCAT(ol.product_code SEPARATOR ', ') AS order_contents, SUM(ol.order_line_cost) AS order_cost, o.order_status, a.userID, b.card_number "
-        + "FROM Orders o "
-        + "JOIN Accounts a ON o.userID = a.userID "
-        + "JOIN Addresses ad ON a.userID = ad.userID "
-        + "JOIN OrderLines ol ON o.order_number = ol.order_number "
-        + "LEFT JOIN BankDetails b ON a.userID = b.userID "
-        + "WHERE o.order_status = 'c' "
-        + "GROUP BY o.order_number, o.order_date, a.forename, a.surname, a.email_address, ad.house_number, ad.street_name, ad.city_name, ad.postcode, o.order_status, a.userID, b.card_number";
-
+                + "GROUP_CONCAT(ol.product_code SEPARATOR ', ') AS order_contents, SUM(ol.order_line_cost) AS order_cost, o.order_status, a.userID, b.card_number "
+                + "FROM Orders o "
+                + "JOIN Accounts a ON o.userID = a.userID "
+                + "JOIN Addresses ad ON a.userID = ad.userID "
+                + "JOIN OrderLines ol ON o.order_number = ol.order_number "
+                + "LEFT JOIN BankDetails b ON a.userID = b.userID "
+                + "WHERE o.order_status = 'c' "
+                + "GROUP BY o.order_number, o.order_date, a.forename, a.surname, a.email_address, ad.house_number, ad.street_name, ad.city_name, ad.postcode, o.order_status, a.userID, b.card_number";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -97,19 +98,21 @@ public class QueueView extends JPanel {
     private String getAddressString(ResultSet resultSet) throws SQLException {
         // Construct and return the postal address as a string
         return resultSet.getInt("house_number") + " " +
-               resultSet.getString("street_name") + ", " +
-               resultSet.getString("city_name") + ", " +
-               resultSet.getString("postcode");
+                resultSet.getString("street_name") + ", " +
+                resultSet.getString("city_name") + ", " +
+                resultSet.getString("postcode");
     }
+
     private void deleteOrder(JTable table) {
         int[] selectedRows = table.getSelectedRows();
-    
+
         if (selectedRows.length > 0) { // Check if at least one row is selected
             List<Integer> orderNumbers = new ArrayList<>();
-    
+
             // Confirm the deletion with a dialog box
-            int option = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete the selected orders?", "Confirm Deletion", JOptionPane.YES_NO_OPTION);
-    
+            int option = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete the selected orders?",
+                    "Confirm Deletion", JOptionPane.YES_NO_OPTION);
+
             if (option == JOptionPane.YES_OPTION) {
                 // User confirmed deletion
                 // Collect order numbers for selected rows
@@ -117,7 +120,7 @@ public class QueueView extends JPanel {
                     int orderNumber = (int) table.getValueAt(selectedRow, 0);
                     orderNumbers.add(orderNumber);
                 }
-    
+
                 // Implement the logic to delete the selected orders
                 try {
                     // Example SQL query to delete multiple orders
@@ -126,66 +129,64 @@ public class QueueView extends JPanel {
                     try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
                         preparedStatement.executeUpdate();
                     }
-    
+
                     // Refresh the table after deletion
                     refreshTable(table);
-    
+
                     // Display a confirmation message
-                    JOptionPane.showMessageDialog(this, "Selected orders deleted successfully.", "Deletion Successful", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(this, "Selected orders deleted successfully.", "Deletion Successful",
+                            JOptionPane.INFORMATION_MESSAGE);
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
         } else {
-            JOptionPane.showMessageDialog(this, "Please select at least one row to delete.", "Delete Order", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Please select at least one row to delete.", "Delete Order",
+                    JOptionPane.WARNING_MESSAGE);
         }
     }
-    
-    private void changeOrderStatus(JTable table) {
-        int[] selectedRows = table.getSelectedRows();
 
+    private void fulfillOrder(JTable table) {
+        int[] selectedRows = table.getSelectedRows();
+    
         if (selectedRows.length > 0) { // Check if at least one row is selected
             List<Integer> orderNumbers = new ArrayList<>();
-
-            // Prompt the user for the new order status
-            String newStatus = JOptionPane.showInputDialog(this, "Enter new status (p, c, or f):");
-
-            if (newStatus != null && (newStatus.equals("p") || newStatus.equals("c") || newStatus.equals("f"))) {
-                // Collect order numbers for selected rows
-                for (int selectedRow : selectedRows) {
-                    int orderNumber = (int) table.getValueAt(selectedRow, 0);
-                    orderNumbers.add(orderNumber);
-                }
-
-                // Implement the logic to change the status of the selected orders
+    
+            // Confirm order fulfillment
+            int confirmResult = JOptionPane.showConfirmDialog(this,
+                    "Are you sure you want to fulfill the selected order(s)?", "Fulfill Order Confirmation",
+                    JOptionPane.YES_NO_OPTION);
+    
+            if (confirmResult == JOptionPane.YES_OPTION) {
+                // Implement the logic to change the status of the selected orders to "f" (fulfilled)
                 try {
                     // Example SQL query to update the order status for multiple orders
-                    String updateQuery = "UPDATE Orders SET order_status = ? WHERE order_number IN (" +
+                    String updateQuery = "UPDATE Orders SET order_status = 'f' WHERE order_number IN (" +
                             String.join(",", orderNumbers.stream().map(String::valueOf).toArray(String[]::new)) + ")";
                     try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
-                        preparedStatement.setString(1, newStatus);
                         preparedStatement.executeUpdate();
                     }
-
+    
+                    // Display a success message
+                    JOptionPane.showMessageDialog(this, "Order(s) fulfilled successfully!", "Fulfill Order",
+                            JOptionPane.INFORMATION_MESSAGE);
+    
                     // Refresh the table after status change
                     refreshTable(table);
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
-            } else {
-                JOptionPane.showMessageDialog(this, "Invalid status. Please enter p, c, or f.", "Change Order Status",
-                        JOptionPane.WARNING_MESSAGE);
             }
         } else {
-            JOptionPane.showMessageDialog(this, "Please select at least one row to change the status.",
-                    "Change Order Status", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Please select at least one row to fulfill the order.",
+                    "Fulfill Order", JOptionPane.WARNING_MESSAGE);
         }
     }
-
+    
     private void refreshTable(JTable table) {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0); // Clear existing rows
-    
+
         // SQL query to retrieve confirmed orders with relevant information
         String sqlQuery = "SELECT o.order_number, o.order_date, a.forename, a.surname, a.email_address, ad.house_number, ad.street_name, ad.city_name, ad.postcode, "
                 + "GROUP_CONCAT(ol.product_code SEPARATOR ', ') AS order_contents, SUM(ol.order_line_cost) AS order_cost, o.order_status, a.userID, b.card_number "
@@ -196,7 +197,7 @@ public class QueueView extends JPanel {
                 + "LEFT JOIN BankDetails b ON a.userID = b.userID "
                 + "WHERE o.order_status = 'confirmed' "
                 + "GROUP BY o.order_number, o.order_date, a.forename, a.surname, a.email_address, ad.house_number, ad.street_name, ad.city_name, ad.postcode, o.order_status, a.userID, b.card_number";
-    
+
         table.setModel(createTableFromSQL().getModel()); // Populate the table with updated data
     }
 
