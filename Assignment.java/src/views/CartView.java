834e7818b105399e1cf06b18a47e8df4e3949c38
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-11-29 15:13:58 +0000

    Implemented previous orders view in CartView

--- Assignment.java/src/views/CartView.java
+++ Assignment.java/src/views/CartView.java
@@ -3,7 +3,6 @@ package src.views;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,79 +10,101 @@ import java.sql.SQLException;
 
 public class CartView extends JFrame {
     private Connection connection;
+
+    private JTable currentOrdersTable;
+    private JTable fulfilledOrdersTable;
+
     private CardLayout cardLayout;
     private JPanel cardPanel;
-    private int orderNumber; // Assuming each CartView is associated with a unique order_number
-    private JTable table;
 
-    public CartView(Connection connection, int orderNumber)
-            throws SQLException {
+    public CartView(Connection connection) {
         // Create the JFrame in the constructor
-        this.setTitle("Cart");
+        this.setTitle("Staff Dashboard");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 500);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
+        
         this.connection = connection;
-        this.orderNumber = orderNumber;
 
         setLayout(new BorderLayout());
-
-        // Initialize CardLayout and JPanel
-        cardLayout = new CardLayout();
-        cardPanel = new JPanel(cardLayout);
-
-        // Content for Cart Screen
-        add(new JLabel("Cart Screen Content", SwingConstants.CENTER));
 
         // Back button to the initial screen
         JButton backButton = new JButton("Back to Main Screen");
         backButton.addActionListener(e -> cardLayout.show(cardPanel, "Initial"));
 
-        // Table with data from the SQL table OrderLines
-        table = createTableFromSQL();
-        JScrollPane scrollPane = new JScrollPane(table);
+        // Table with data from the current orders
+        currentOrdersTable = createTableFromSQL("c");
+        JScrollPane currentOrdersScrollPane = new JScrollPane(currentOrdersTable);
 
-        // Buttons
-        
-        JButton editQuantityButton = new JButton("Edit Quantity");
-        editQuantityButton.addActionListener(this::editQuantity);
-        
-        JButton removeButton = new JButton("Remove");
-        removeButton.addActionListener(this::removeOrderLine);
-        
+        // Table with data from fulfilled orders
+        fulfilledOrdersTable = createTableFromSQL("f");
+        JScrollPane fulfilledOrdersScrollPane = new JScrollPane(fulfilledOrdersTable);
+
+        // Header for current orders table
+        JLabel currentOrdersHeader = new JLabel("Cart", SwingConstants.CENTER);
+        JPanel currentOrdersPanel = new JPanel(new BorderLayout());
+        currentOrdersPanel.add(currentOrdersHeader, BorderLayout.NORTH);
+        currentOrdersPanel.add(currentOrdersScrollPane, BorderLayout.CENTER);
+
+        // Header for fulfilled orders table
+        JLabel fulfilledOrdersHeader = new JLabel("Previous Orders", SwingConstants.CENTER);
+        JPanel fulfilledOrdersPanel = new JPanel(new BorderLayout());
+        fulfilledOrdersPanel.add(fulfilledOrdersHeader, BorderLayout.NORTH);
+        fulfilledOrdersPanel.add(fulfilledOrdersScrollPane, BorderLayout.CENTER);
+
+        // Buttons for Cart actions (confirm, edit quantity, remove)
         JButton confirmOrderButton = new JButton("Confirm Order");
-        confirmOrderButton.addActionListener(this::confirmOrder);
+        JButton editQuantityButton = new JButton("Edit Quantity");
+        JButton removeOrderButton = new JButton("Remove Order");
 
+        // Add action listeners to buttons
+        confirmOrderButton.addActionListener(e -> confirmOrder());
+        editQuantityButton.addActionListener(e -> editQuantity());
+        removeOrderButton.addActionListener(e -> removeOrder());
 
+        // Add components to the panel
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(backButton);
-        buttonPanel.add(removeButton);
-        buttonPanel.add(editQuantityButton);
         buttonPanel.add(confirmOrderButton);
+        buttonPanel.add(editQuantityButton);
+        buttonPanel.add(removeOrderButton);
 
-        add(scrollPane, BorderLayout.CENTER);
-        add(buttonPanel, BorderLayout.SOUTH);
+        // Split the screen into two halves with tables
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, currentOrdersPanel, fulfilledOrdersPanel);
+        splitPane.setResizeWeight(0.5);
+
+        // Add components to the panel
+        add(splitPane, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);    
     }
 
-    private JTable createTableFromSQL() {
-        String[] columnNames = {"Order Line Number", "Item Quantity", "Order Line Cost", "Product Code", "Order Number"};
+    private JTable createTableFromSQL(String orderStatus) {
+        String[] columnNames = {"Order Number", "Date", "Order Contents", "Order Cost", "Order Status"};
+
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
 
-        // SQL query to retrieve data from the OrderLines table for a specific order_number
-        String sqlQuery = "SELECT order_line_number, items_quantity, order_line_cost, product_code, order_number FROM OrderLines WHERE order_number = ?";
+        // SQL query to retrieve orders with the specified status
+        String sqlQuery = "SELECT o.order_number, o.order_date, GROUP_CONCAT(ol.product_code SEPARATOR ', ') AS order_contents, "
+                + "SUM(ol.order_line_cost) AS order_cost, o.order_status "
+                + "FROM Orders o "
+                + "JOIN OrderLines ol ON o.order_number = ol.order_number "
+                + "WHERE o.userID = ? AND o.order_status = ? "
+                + "GROUP BY o.order_number, o.order_date, o.order_status";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-            preparedStatement.setInt(1, orderNumber);
+            // TODO Replace with function to get the actual user ID
+            preparedStatement.setString(1, "4583ef08-25fe-4038-90d0-b1a3523c099d"); // Placeholder user ID
+            preparedStatement.setString(2, orderStatus);
+
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
                 Object[] row = {
-                        resultSet.getInt("order_line_number"),
-                        resultSet.getInt("items_quantity"),
-                        resultSet.getDouble("order_line_cost"),
-                        resultSet.getString("product_code"),
-                        resultSet.getInt("order_number")
+                        resultSet.getInt("order_number"),
+                        resultSet.getDate("order_date"),
+                        resultSet.getString("order_contents"),
+                        resultSet.getDouble("order_cost"),
+                        resultSet.getString("order_status")
                 };
                 model.addRow(row);
             }
@@ -94,116 +115,69 @@ public class CartView extends JFrame {
         return new JTable(model);
     }
 
+    private void confirmOrder() {
+        // Implement the logic for confirming orders
+        int selectedRow = currentOrdersTable.getSelectedRow();
+        if (selectedRow != -1) {
+            // Assuming orderNumber is the first column in the table
+            int orderNumber = (int) currentOrdersTable.getValueAt(selectedRow, 0);
+            System.out.println("Order confirmed: " + orderNumber);
 
-    private void confirmOrder(ActionEvent event) {
-        // Check if there are any order lines in the cart
-        if (table.getRowCount() > 0) {
-            try {
-                // Confirm the order in the database (update the order status, for example)
-                String updateQuery = "UPDATE Orders SET order_status = 'confirmed' WHERE order_number = ?";
-                try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
-                    preparedStatement.setInt(1, orderNumber);
-                    preparedStatement.executeUpdate();
-                }
-    
-                // Show confirmation message
-                JOptionPane.showMessageDialog(this, "Order confirmed successfully.", "Confirmation", JOptionPane.INFORMATION_MESSAGE);
-    
-                // Refresh the table after confirmation
-                refreshTable(table);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            // Perform database update (modify as needed)
+            updateOrderStatus(orderNumber, "f");
         } else {
-            // Show a pop-up message indicating that the cart is empty
-            JOptionPane.showMessageDialog(this, "The cart is empty. Add items to the cart before confirming.", "Empty Cart", JOptionPane.WARNING_MESSAGE);
-        }
-    }
-    
-    private void editQuantity(ActionEvent event) {
-        // Get the selected row from the table
-        int selectedRow = table.getSelectedRow();
-    
-        if (selectedRow != -1) { // Check if a row is selected
-            try {
-                // Prompt the user for the new quantity
-                String newQuantityStr = JOptionPane.showInputDialog(this, "Enter new quantity:");
-                if (newQuantityStr != null) {
-                    int newQuantity = Integer.parseInt(newQuantityStr);
-    
-                    // Update the quantity in the database
-                    String updateQuery = "UPDATE OrderLines SET items_quantity = ? WHERE order_line_number = ?";
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
-                        preparedStatement.setInt(1, newQuantity);
-                        preparedStatement.setInt(2, (int) table.getValueAt(selectedRow, 0));
-                        preparedStatement.executeUpdate();
-                    }
-    
-                    // Show success message
-                    JOptionPane.showMessageDialog(this, "Quantity updated successfully.", "Edit Quantity", JOptionPane.INFORMATION_MESSAGE);
-    
-                    // Refresh the table after updating quantity
-                    refreshTable(table);
-                }
-            } catch (NumberFormatException | SQLException e) {
-                e.printStackTrace();
-            }
-        } else {
-            JOptionPane.showMessageDialog(this, "Please select a row to edit quantity.", "Edit Quantity", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Please select an order to confirm.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    private void removeOrderLine(ActionEvent event) {
-        // Get the selected row from the table
-        int selectedRow = table.getSelectedRow();
-    
-        if (selectedRow != -1) { // Check if a row is selected
-            try {
-                // Get the order line number
-                int orderLineNumber = (int) table.getValueAt(selectedRow, 0);
-    
-                // Delete the order line from the database
-                String deleteQuery = "DELETE FROM OrderLines WHERE order_line_number = ?";
-                try (PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery)) {
-                    preparedStatement.setInt(1, orderLineNumber);
-                    preparedStatement.executeUpdate();
-                }
-    
-                // Show success message
-                JOptionPane.showMessageDialog(this, "Order line removed successfully.", "Remove Order Line", JOptionPane.INFORMATION_MESSAGE);
-    
-                // Refresh the table after removing order line
-                refreshTable(table);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+    private void editQuantity() {
+        // Implement the logic for editing order quantity
+        int selectedRow = currentOrdersTable.getSelectedRow();
+        if (selectedRow != -1) {
+            // Assuming orderNumber is the first column in the table
+            int orderNumber = (int) currentOrdersTable.getValueAt(selectedRow, 0);
+            System.out.println("Editing quantity for order: " + orderNumber);
+
+            // Perform database update (modify as needed)
+            // For example, open a dialog for quantity input and update the database
         } else {
-            JOptionPane.showMessageDialog(this, "Please select a row to remove.", "Remove Order Line", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Please select an order to edit quantity.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    private void refreshTable(JTable table) {
-        // Clear the existing rows in the table
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        model.setRowCount(0);
-    
-        // Fetch and populate the table with updated data from the OrderLines table
-        String sqlQuery = "SELECT order_line_number, items_quantity, order_line_cost, product_code, order_number FROM OrderLines WHERE order_number = ?";
-    
+    private void removeOrder() {
+        // Implement the logic for removing orders
+        int selectedRow = currentOrdersTable.getSelectedRow();
+        if (selectedRow != -1) {
+            // Assuming orderNumber is the first column in the table
+            int orderNumber = (int) currentOrdersTable.getValueAt(selectedRow, 0);
+            System.out.println("Removing order: " + orderNumber);
+
+            // Perform database update (modify as needed)
+            removeOrderFromDatabase(orderNumber);
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select an order to remove.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void updateOrderStatus(int orderNumber, String newStatus) {
+        // Placeholder method for updating order status in the database (modify as needed)
+        String sqlQuery = "UPDATE Orders SET order_status = ? WHERE order_number = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            preparedStatement.setString(1, newStatus);
+            preparedStatement.setInt(2, orderNumber);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void removeOrderFromDatabase(int orderNumber) {
+        // Placeholder method for removing order from the database (modify as needed)
+        String sqlQuery = "DELETE FROM Orders WHERE order_number = ?";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
             preparedStatement.setInt(1, orderNumber);
-            ResultSet resultSet = preparedStatement.executeQuery();
-    
-            while (resultSet.next()) {
-                Object[] row = {
-                        resultSet.getInt("order_line_number"),
-                        resultSet.getInt("items_quantity"),
-                        resultSet.getDouble("order_line_cost"),
-                        resultSet.getString("product_code"),
-                        resultSet.getInt("order_number")
-                };
-                model.addRow(row);
-            }
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
