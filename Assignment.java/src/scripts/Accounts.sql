71a843d6ab1b8a2aa59bb1dcd87a6fe3f06ef48b
NawarUoS <naboutrabi1@sheffield.ac.uk>
2023-12-01 16:46:31 +0000

    Updated Accounts.sql

--- Assignment.java/src/scripts/Accounts.sql
+++ Assignment.java/src/scripts/Accounts.sql
@@ -3,7 +3,10 @@ CREATE TABLE Accounts (
     forename text(30) NOT NULL,
     surname text(30) NOT NULL,
     email_address text(254) NOT NULL,
-    password text NOT NULL,
+    unique_password_hash text(254) NOT NULL,
+    user_customer tinyint(1) NOT NULL,
+    user_staff tinyint(1) NOT NULL,
+    user_manager tinyint(1) NOT NULL,
     PRIMARY KEY (userID)
 );
     
