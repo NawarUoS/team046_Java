71598dd8d311316a290c69d4fc7ee9f20a0a9ee4
Ahmad Alenezi <aalenezi1@sheffield.ac.uk>
2023-12-01 16:22:38 +0000

    fixed again

--- Assignment.java/src/scripts/OrderLines.sql
+++ Assignment.java/src/scripts/OrderLines.sql
@@ -1,10 +1,10 @@
-CREATE TABLE OrderLinessss (
-PRIMARY KEY (order_line_number),
-order_line_number INT NOT NULL,
-items_quantity INT NOT NULL,
-order_line_cost DOUBLE NOT NULL,
-product_code VARCHAR (6) NOT NULL,
-order_number INT NOT NULL,
-FOREIGN KEY (product_code) REFERENCES Products(product_code),
-FOREIGN KEY (order_number) REFERENCES Orders(order_number)
+CREATE TABLE OrderLines (
+    order_line_number INT NOT NULL,
+    items_quantity INT NOT NULL,
+    order_line_cost DOUBLE NOT NULL,
+    product_code VARCHAR(6) NOT NULL,
+    order_number INT NOT NULL,
+    PRIMARY KEY (order_line_number, order_number),
+    FOREIGN KEY (product_code) REFERENCES Products(product_code),
+    FOREIGN KEY (order_number) REFERENCES Orders(order_number)
 );
